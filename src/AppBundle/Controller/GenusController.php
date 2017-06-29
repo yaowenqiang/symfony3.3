4f420bf78b61f9238ac7ebe12061ff3844164b87
yaowenqiang <yaowenqiang111@163.com>
2017-06-30 01:36:38 +0800

    add Wildcard routes

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -15,10 +15,11 @@ use \Symfony\Component\HttpFoundation\Response;
 class GenusController
 {
     /**
-     * @Route("/genus")
+     * @Route("/genus/{genusName}")
      */
-    public function showAction()
+    public function showAction($genusName)
     {
-        return new  Response("Under the Sea!");
+        //octopus
+        return new  Response("The Genus: ".$genusName);
     }
 }
