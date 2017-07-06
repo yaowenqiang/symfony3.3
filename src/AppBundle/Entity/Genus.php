fa9669d96295327f9a2cc4af25717ed2459da8ef
yaowenqiang <yaowenqiang111@163.com>
2017-07-07 01:08:11 +0800

    add the genus model via annotation

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leaphu
+ * Date: 2017/7/7
+ * Time: 上午1:00
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\ as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
+class Genus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+    
+
+}
