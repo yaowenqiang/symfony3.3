65d1fcb79b4d9f64ec43c060d383264eb99d8e05
yaowenqiang <yaowenqiang111@163.com>
2017-07-12 02:05:05 +0800

    add the nelmio/alice fixtures demo

--- src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -11,19 +11,12 @@ namespace AppBundle\DataFixtures\ORM;
 use AppBundle\Entity\Genus;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Fixtures;
 
 class LoadFixtures implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-
-
-        $genus = new Genus();
-        $genus->setName('Octopus'.rand(1,100));
-        $genus->setSubFaminy('Octopodinae');
-        $genus->setSpeciesCount(rand(100,99999));
-
-        $manager->persist($genus);
-        $manager->flush();
+        Fixtures::load(__DIR__.'/fixtures.yml',$manager);
     }
 }
