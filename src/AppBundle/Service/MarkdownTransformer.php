390c5eb55cc550596ff9a908a0d117f3e75c861d
yaowenqiang <yaowenqiang111@163.com>
2017-07-23 08:56:46 +0800

    add argument type-hinting

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -9,10 +9,12 @@
 namespace AppBundle\Service;
 
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+
 class MarkdownTransformer
 {
     private $markdownParser;
-    public  function __construct($markdownParser)
+    public  function __construct(MarkdownParserInterface $markdownParser)
     {
         $this->markdownParser = $markdownParser;
     }
